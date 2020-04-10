2685ff35342b9b36baf6de8cdf0e5ef75431e45f
Gergo Ility <gergo_ility@epam.com>
2020-04-10 07:43:10 +0200

    Joi validation for create/update user

--- homework_2/src/routes/userRoutes.ts
+++ homework_2/src/routes/userRoutes.ts
@@ -1,11 +1,18 @@
 import { Router, Request, Response, NextFunction } from 'express'
+import { createValidator } from 'express-joi-validation'
 import userController from '../controllers/userController'
+import userJoi from '../validators/userValidation'
 
 const router = Router()
+const validator = createValidator()
 
-router.post('/', (req: Request, res: Response, next: NextFunction) => {
-    userController.create(req, res, next)
-})
+router.post(
+    '/',
+    validator.body(userJoi),
+    (req: Request, res: Response, next: NextFunction) => {
+        userController.create(req, res, next)
+    }
+)
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     userController.getAll(req, res, next)
@@ -15,9 +22,13 @@ router.get('/:id', (req: Request, res: Response, next: NextFunction) => {
     userController.getById(req, res, next)
 })
 
-router.put('/:id', (req: Request, res: Response, next: NextFunction) => {
-    userController.update(req, res, next)
-})
+router.put(
+    '/:id',
+    validator.body(userJoi),
+    (req: Request, res: Response, next: NextFunction) => {
+        userController.update(req, res, next)
+    }
+)
 
 router.delete('/:id', (req: Request, res: Response, next: NextFunction) => {
     userController.remove(req, res, next)
---
+++ homework_2/src/validators/userValidation.ts
@@ -0,0 +1,18 @@
+import Joi from '@hapi/joi'
+import PasswordComplexity from 'joi-password-complexity'
+
+const passwordComplexityOptions = {
+    min: 3,
+    max: 255,
+    lowerCase: 1,
+    upperCase: 1,
+    numeric: 1,
+    symbol: 0,
+}
+
+export default Joi.object({
+    login: Joi.string().required(),
+    password: PasswordComplexity(passwordComplexityOptions),
+    age: Joi.number().integer().min(4).max(130).required(),
+    isDeleted: Joi.boolean().required(),
+})
