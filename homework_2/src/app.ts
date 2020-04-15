8886d3b7a77ed5f4e1f03e0e2c57a3b78eb8eddc
Gergo Ility <gergo_ility@epam.com>
2020-04-15 13:16:27 +0200

    Database connection with sequalize

--- homework_2/src/app.ts
+++ homework_2/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import userRouter from './routes/userRoutes'
+import database from './models/database'
 
 const app = express()
 const port = 5000
@@ -13,5 +14,6 @@ app.listen(port, (err) => {
         return console.error(err)
     }
 
+    database.connect()
     return console.log(`Server is listening on ${port}`)
 })
---
+++ homework_2/src/models/database.ts
@@ -0,0 +1,21 @@
+import { Sequelize } from 'sequelize'
+
+const sequelize = new Sequelize(
+    'postgres://admin:admin@database:5432/mentoring'
+)
+
+const connect = (): void => {
+    sequelize
+        .authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.')
+        })
+        .catch((err: Error) => {
+            console.error('Unable to connect to the database:', err)
+        })
+}
+
+export default {
+    sequelize,
+    connect,
+}
